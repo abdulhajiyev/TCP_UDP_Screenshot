dfef1fdecc5dfaacfe09706cb6897e616eada3d5
Abdul Hajiyev <abdulhajiyev@gmail.com>
2022-01-31 21:31:53 +0400

    .

--- SS_Client/Form1.Designer.cs
+++ SS_Client/Form1.Designer.cs
@@ -38,7 +38,7 @@ namespace SS_Client
             // 
             this.pictureBox1.Location = new System.Drawing.Point(12, 41);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(415, 237);
+            this.pictureBox1.Size = new System.Drawing.Size(865, 428);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -47,7 +47,7 @@ namespace SS_Client
             // 
             this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(415, 23);
+            this.button1.Size = new System.Drawing.Size(865, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Connect";
             this.button1.UseVisualStyleBackColor = true;
@@ -57,7 +57,7 @@ namespace SS_Client
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(443, 292);
+            this.ClientSize = new System.Drawing.Size(889, 481);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
