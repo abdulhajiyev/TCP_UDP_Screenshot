c5fedfff5661c33c120b04971b7ba921f005f994
Haci_cdd9 <Haci_cdd9@STHQ0116-04>
2022-01-31 19:21:43 +0400

    Add project files.

---
+++ SS_Server/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SS_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ip = IPAddress.Loopback;
+            var listener = new TcpListener(ip, 9290);
+
+
+            listener.Start(100);
+            Console.WriteLine("Listening on {0}", listener.LocalEndpoint);
+        }
+    }
+}
