c17ccfeb27440c165e51f8333351a40cd0566097
Abdul Hajiyev <abdulhajiyev@gmail.com>
2022-01-31 20:14:52 +0400

    .

--- SS_Client/Form1.cs
+++ SS_Client/Form1.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,5 +20,25 @@ namespace SS_Client
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var client = new TcpClient();
+            client.Connect(IPAddress.Loopback, 9290);
+
+            var UdpClient = new UdpClient(9590);
+            var ep = new IPEndPoint(IPAddress.Loopback, 0);
+
+            for (int i = 0; i < 10; i++)
+            {
+                var bytes = UdpClient.Receive(ref ep);
+                Image img;
+
+                using (var ms = new MemoryStream(bytes))
+                {
+                    img = Image.FromStream(ms);
+                }
+                pictureBox1.Image = img;
+            }
+        }
     }
 }
--- SS_Server/Program.cs
+++ SS_Server/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
+
 
 namespace SS_Server
 {
@@ -8,12 +12,44 @@ namespace SS_Server
     {
         static void Main(string[] args)
         {
-            var ip = IPAddress.Loopback;
-            var listener = new TcpListener(ip, 9290);
-
+            var listener = new TcpListener(IPAddress.Loopback, 9290);
 
             listener.Start(100);
             Console.WriteLine("Listening on {0}", listener.LocalEndpoint);
+
+            while (true)
+            {
+                listener.AcceptTcpClient();
+                Console.WriteLine("Client connected");
+
+                using var bitmap = new Bitmap(1920, 1080);
+                using (var g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(0, 0, 0, 0,
+                        bitmap.Size, CopyPixelOperation.SourceCopy);
+                }
+                bitmap.Save("screenshot.jpg", ImageFormat.Jpeg);
+
+                UdpClient udpClient = new UdpClient();
+                try
+                {
+                    udpClient.Connect(IPAddress.Loopback, 9590);
+
+                    // Sends a message to the host to which you have connected.
+
+                    byte[] buffer = File.ReadAllBytes("screenshot.jpg");
+                    udpClient.Send(buffer, buffer.Length);
+
+                    udpClient.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+
+                //bitmap.Save("screenshot.jpg", ImageFormat.Jpeg);
+
+            }
         }
     }
 }
